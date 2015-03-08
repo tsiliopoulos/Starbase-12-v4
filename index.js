cd70c2a4e7d2f7182cb8ed860856a2337159fa1b
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2015-03-07 21:45:00 -0500

    updated index.js

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+
+// we are specifying the html directory as another public directory
+app.use(express.static(path.join(__dirname, 'Starbase12-v4')));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendfile('index.html');
 })
 
 var server = app.listen(process.env.PORT || 3000, function () {
